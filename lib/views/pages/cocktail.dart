0d381a9276caab9eda6b156ff86976fb6e5b3efc
Iman Irajdoost <iman@seven-shapes.com>
2023-11-15 10:05:50 +0100

    added cocktail page

--- lib/views/pages/cocktail.dart
+++ lib/views/pages/cocktail.dart
@@ -13,12 +13,53 @@ class CocktailPage extends StatefulWidget {
 class _CocktailPageState extends State<CocktailPage> {
   @override
   Widget build(BuildContext context) {
+    // Determine the shorter length of the two lists
+    int listLength = widget.cocktail.ingredients.length;
+    if (widget.cocktail.measures.length < listLength) {
+      listLength = widget.cocktail.measures.length;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.cocktail.name),
       ),
-      body: Center(
-        child: Text('Details for ${widget.cocktail.name}'),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            // Display the thumbnail image if available
+            Image.network(widget.cocktail.thumbnailURL),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // Display the cocktail name if available
+                  Text(
+                    widget.cocktail.name ?? 'Unknown Cocktail',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  const SizedBox(height: 10),
+                  // Display ingredients and their measures if available
+                  if (listLength > 0)
+                    Text(
+                      'Ingredients:',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  for (int i = 0; i < listLength; i++)
+                    Text('${widget.cocktail.ingredients[i]} - ${widget.cocktail.measures.isNotEmpty ? widget.cocktail.measures[i] : 'N/A'}'),
+                  const SizedBox(height: 20),
+                  // Display the instructions if available
+                  Text(
+                    'Instructions:',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  Text(widget.cocktail.instructions),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
