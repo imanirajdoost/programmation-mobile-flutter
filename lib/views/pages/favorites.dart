f345327e25f133b06ddd8392084c85341470f54b
Iman Irajdoost <iman@seven-shapes.com>
2023-11-15 10:38:02 +0100

    added share button

--- lib/views/pages/favorites.dart
+++ lib/views/pages/favorites.dart
@@ -11,11 +11,15 @@ class _FavoritePageState extends State<FavoritePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.count(crossAxisCount: 2,
+      appBar: AppBar(
+        title: const Text('Favorite Cocktails'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
         children: List.generate(20, (index) {
           return const Center(
-            child: Divider()//CocktailListItem(cocktail: null),
-          );
+              child: Divider() //CocktailListItem(cocktail: null),
+              );
         }),
       ),
     );
