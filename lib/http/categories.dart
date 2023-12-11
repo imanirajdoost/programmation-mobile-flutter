01f14be929c03d438e1aa7a5b31d55a4c56246c4
Vincent WENDLING <vincent.wendling@lilo.org>
2023-12-11 16:19:04 +0100

    dsfsdfsdf

---
+++ lib/http/categories.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future<List<String>> fetchCocktails() async {
+
+  List<String> categories = [];
+
+  final response = await http.get(Uri.parse('www.thecocktaildb.com/api/json/v1/1/list.php?c=list'));
+
+  if (response.statusCode == 200) {
+    var array = jsonDecode(response.body)['drinks'];
+    array.forEach((category) => categories.add(category['strCategory']));
+  } else {
+    throw Exception("Couldn't fetch categories");
+  }
+
+  return categories;
+}
